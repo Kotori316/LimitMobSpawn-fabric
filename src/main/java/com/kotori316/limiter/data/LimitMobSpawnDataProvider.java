6244b2f4540d3c42ec347d10b3aabc1f8495bbf6
Kotori316 <Kotori316@gmail.com>
2022-02-25 10:52:25 +0900

    Data generator

--- src/main/java/com/kotori316/limiter/data/LimitMobSpawnDataProvider.java
+++ src/main/java/com/kotori316/limiter/data/LimitMobSpawnDataProvider.java
@@ -8,6 +8,8 @@ import java.util.List;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.DataProvider;
 import net.minecraft.data.HashCache;
@@ -15,8 +17,12 @@ import org.apache.commons.lang3.tuple.Pair;
 
 import com.kotori316.limiter.LimitMobSpawn;
 
-@SuppressWarnings("unused") // Currently, no way to generate data files.
-public class LimitMobSpawnDataProvider {
+public final class LimitMobSpawnDataProvider implements DataGeneratorEntrypoint {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+        fabricDataGenerator.addProvider(TestSpawnProvider::new);
+    }
+
     private record TestSpawnProvider(DataGenerator dataGenerator) implements DataProvider {
 
         @Override
